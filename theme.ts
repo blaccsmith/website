3f8ce9a6cb7926c0a88bbda33e467a1aa5785457
KP <garrett1.tolbert@gmail.com>
2021-08-18 22:38:19 -0500

    added random colors

---
+++ theme.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+	colors: {
+		background: '#212121',
+		purple: { 900: '#7B61FF' },
+		black: { 900: '#333333' },
+		gray: { 900: '#909090' },
+	},
+});
