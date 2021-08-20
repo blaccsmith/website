e7addb8ddb924c10ee7599f8bdc7cf67f0f3a4bf
KP <garrett1.tolbert@gmail.com>
2021-08-19 21:16:17 -0500

    using brand black

--- components/atoms/Layout/Footer.tsx
+++ components/atoms/Layout/Footer.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 export default function Footer(): JSX.Element {
 	return (
-		<Flex p="12" w="full" bg="black" alignItems="center">
+		<Flex p="12" w="full" bg="brand.black" alignItems="center">
 			<Image width={50} height={50} src="/logo-light.png" />
 			<Text ml="8" color="#fff">
 				This is the footer 😅
