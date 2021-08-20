ee7c14e291265473b11c96d03568676634a06b09
KP <garrett1.tolbert@gmail.com>
2021-08-19 20:36:02 -0500

    updated styles with colors, fonts and font sizes

--- theme.ts
+++ theme.ts
@@ -1,19 +1,32 @@
 import { extendTheme } from '@chakra-ui/react';
 
 export const theme = extendTheme({
-	colors: {
-		purple: {
-			900: '#7B61FF',
-		},
-		black: {
-			900: '#333333',
-		},
-		gray: {
-			900: '#909090',
-		},
-	},
 	fonts: {
-		heading: 'Rubik',
-		body: 'Inter',
+		heading: 'Raleway',
+		body: 'Montserrat',
+		subtitle: 'Lato',
+	},
+	fontSizes: {
+		xs: '0.64rem',
+		sm: '0.8rem',
+		md: '1rem',
+		lg: '1.25rem',
+		xl: '1.563rem',
+		'2xl': '1.953rem',
+		'3xl': '2.441rem',
+		'4xl': '3.052rem',
+		'5xl': '3.815rem',
+		'6xl': '4.768rem',
+		'7xl': '5.960rem',
+		'8xl': '7.451rem',
+		'9xl': '9.313rem',
+	},
+	colors: {
+		brand: {
+			white: '#f5f5f5',
+			black: '#212121',
+			translucent: 'rgba(33, 33, 33, 0.65)',
+			accent: { light: '#6200EA', dark: '#B388FF' },
+		},
 	},
 });
