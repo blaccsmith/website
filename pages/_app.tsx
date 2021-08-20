3d745430d99d23ec3cda30e5f7bc1f1f6cf92d1c
KP <garrett1.tolbert@gmail.com>
2021-08-19 20:36:24 -0500

    created sandbox layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,15 @@
+import '@fontsource/montserrat/300.css';
+import '@fontsource/montserrat/400.css';
+import '@fontsource/montserrat/500.css';
+
+import '@fontsource/lato/300.css';
+import '@fontsource/lato/400.css';
+import '@fontsource/lato/700.css';
+
+import '@fontsource/raleway/500.css';
+import '@fontsource/raleway/700.css';
+import '@fontsource/raleway/900.css';
+
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '../theme';
