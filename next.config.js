892ce3a595916b1366099e57885e6d0963a9382c
Garrett1Tolbert <garrett1.tolbert@gmail.com>
2021-09-05 21:01:36 +0000

    fix: allow twitter images in app

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 /** @type {import('next').NextConfig} */
 module.exports = {
-	images: { domains: ['images.unsplash.com'] },
+	images: { domains: ['images.unsplash.com', 'twitter.com'] },
 	reactStrictMode: true,
 };
