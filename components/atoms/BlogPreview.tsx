81ca431c44a21789c61d52bd0384702d9394f6c4
Garrett1Tolbert <garrett1.tolbert@gmail.com>
2021-09-11 19:46:09 +0000

    feat: blog previews link to actual blog

--- components/atoms/BlogPreview.tsx
+++ components/atoms/BlogPreview.tsx
@@ -1,7 +1,7 @@
 import { AspectRatio, Text, Box, HStack } from '@chakra-ui/react';
 import React from 'react';
 import { BlogMetadata } from 'types';
-import Image from 'next/image';
+import Link from 'next/link';
 import { MdAccessTime } from 'react-icons/md';
 import ProgressiveImage from './ProgressiveImage';
 
@@ -11,33 +11,45 @@ interface Props {
 }
 
 export default function BlogPreview({ data, timeAgo }: Props) {
+	const slug = data.title
+		.split(' ')
+		.map((el) => el.toLowerCase())
+		.join('-');
+
 	return (
-		<Box>
-			<AspectRatio
-				pos="relative"
-				ratio={16 / 9}
-				overflow="hidden"
-				rounded="xl"
-				maxW="80"
-			>
-				<ProgressiveImage
-					src={data.thumbnail}
-					minW={320}
-					minH={240}
-					objectFit="cover"
-					layout="fill"
-					alt="Blog thumbnail"
-				/>
-			</AspectRatio>
-			<Text my="2" fontSize="xl" color="brand.white" fontWeight="bold">
-				{data.title}
-			</Text>
-			<HStack>
-				<MdAccessTime color="#eee" />
-				<Text my="2" fontSize="md" color="brand.offWhite">
-					{timeAgo}
+		<Link passHref href={`/blog/${slug}`}>
+			<Box as="a">
+				<AspectRatio
+					pos="relative"
+					ratio={16 / 9}
+					overflow="hidden"
+					rounded="xl"
+					maxW="80"
+				>
+					<ProgressiveImage
+						src={data.thumbnail}
+						minW={320}
+						minH={240}
+						objectFit="cover"
+						layout="fill"
+						alt="Blog thumbnail"
+					/>
+				</AspectRatio>
+				<Text
+					my="2"
+					fontSize="xl"
+					color="brand.white"
+					fontWeight="bold"
+				>
+					{data.title}
 				</Text>
-			</HStack>
-		</Box>
+				<HStack>
+					<MdAccessTime color="#eee" />
+					<Text my="2" fontSize="md" color="brand.offWhite">
+						{timeAgo}
+					</Text>
+				</HStack>
+			</Box>
+		</Link>
 	);
 }
