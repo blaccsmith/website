1797f44d36b81637f8c3ae12996e6a207e9ff7ee
Garrett1Tolbert <garrett1.tolbert@gmail.com>
2021-09-11 19:47:41 +0000

    chore: removed eslint disable

--- components/atoms/ProgressiveImage.tsx
+++ components/atoms/ProgressiveImage.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import Image from 'next/image';
 
