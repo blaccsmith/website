7e7424fa0488ec15c4a233b22ae4092b03e40810
Garrett1Tolbert <garrett1.tolbert@gmail.com>
2021-09-11 18:53:56 +0000

    chore: update formatting

--- components/atoms/BlogPreview.tsx
+++ components/atoms/BlogPreview.tsx
@@ -1,25 +1,40 @@
-import { AspectRatio,Text, Box, HStack } from "@chakra-ui/react";
-import React from "react";
-import { BlogMetadata } from "types";
-import Image from 'next/image'
-import { MdAccessTime } from 'react-icons/md'
+import { AspectRatio, Text, Box, HStack } from '@chakra-ui/react';
+import React from 'react';
+import { BlogMetadata } from 'types';
+import Image from 'next/image';
+import { MdAccessTime } from 'react-icons/md';
 
 interface Props {
-    data: BlogMetadata
-    timeAgo: string
+	data: BlogMetadata;
+	timeAgo: string;
 }
 
-export default function BlogPreview({ data, timeAgo }:Props) {
-    return (
-        <Box>
-            <AspectRatio pos='relative' ratio={16/9} overflow='hidden' rounded='xl' maxW='80' >
-                <Image src={data.thumbnail} objectFit='cover' layout='fill' alt='Blog thumbnail' />
-            </AspectRatio>
-            <Text my='2' fontSize='xl' color='brand.white' fontWeight='bold'>{data.title}</Text>
-            <HStack>
-                <MdAccessTime color='#eee' />
-                <Text my='2' fontSize='md' color='brand.offWhite'>{timeAgo}</Text>
-            </HStack>
-        </Box>
-    )
+export default function BlogPreview({ data, timeAgo }: Props) {
+	return (
+		<Box>
+			<AspectRatio
+				pos="relative"
+				ratio={16 / 9}
+				overflow="hidden"
+				rounded="xl"
+				maxW="80"
+			>
+				<Image
+					src={data.thumbnail}
+					objectFit="cover"
+					layout="fill"
+					alt="Blog thumbnail"
+				/>
+			</AspectRatio>
+			<Text my="2" fontSize="xl" color="brand.white" fontWeight="bold">
+				{data.title}
+			</Text>
+			<HStack>
+				<MdAccessTime color="#eee" />
+				<Text my="2" fontSize="md" color="brand.offWhite">
+					{timeAgo}
+				</Text>
+			</HStack>
+		</Box>
+	);
 }
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,32 +1,39 @@
-import BlogPreview from "@/components/atoms/BlogPreview"
-import { Box, Heading,Grid } from "@chakra-ui/react"
-import { getBlog, getBlogPaths } from "mdx"
-import { GetStaticProps } from "next"
-import { BlogMetadata } from "types"
-import { formatDistance } from 'date-fns'
+import BlogPreview from '@/components/atoms/BlogPreview';
+import { Box, Heading, Grid } from '@chakra-ui/react';
+import { getBlog, getBlogPaths } from 'mdx';
+import { GetStaticProps } from 'next';
+import { BlogMetadata } from 'types';
+import { formatDistance } from 'date-fns';
 
 export const getStaticProps: GetStaticProps = async () => {
-    const paths = getBlogPaths()
-    const blogs = await Promise.all(paths.map(async(slug) => await getBlog(slug)))
-    const previews = blogs.map((el) => el.frontMatter)
-    return { props: { previews } }
-}
+	const paths = getBlogPaths();
+	const blogs = await Promise.all(
+		paths.map(async (slug) => await getBlog(slug))
+	);
+	const previews = blogs.map((el) => el.frontMatter);
+	return { props: { previews } };
+};
 
 interface Props {
-    previews: BlogMetadata[]
+	previews: BlogMetadata[];
 }
 
-export default function Blog({ previews }:Props) {
-    const publishedAt = new Date(previews[0].published);
-    const timeAgo = formatDistance(publishedAt, new Date(), { addSuffix: true }) 
-    
-    
-    return (
-        <Box minHeight='calc(100vh - 218px)' p='16'>
-            <Heading as='h1' color='brand.white' fontSize='4xl' mb='12'>Blog</Heading>
-            <Grid templateColumns='repeat(4,1fr)' gap='9'>
-                {previews.map(el => <BlogPreview data={el} key={el.title} timeAgo={timeAgo} />)}
-            </Grid>
-        </Box>
-    )
+export default function Blog({ previews }: Props) {
+	const publishedAt = new Date(previews[0].published);
+	const timeAgo = formatDistance(publishedAt, new Date(), {
+		addSuffix: true,
+	});
+
+	return (
+		<Box minHeight="calc(100vh - 218px)" p="16">
+			<Heading as="h1" color="brand.white" fontSize="4xl" mb="12">
+				Blog
+			</Heading>
+			<Grid templateColumns="repeat(4,1fr)" gap="9">
+				{previews.map((el) => (
+					<BlogPreview data={el} key={el.title} timeAgo={timeAgo} />
+				))}
+			</Grid>
+		</Box>
+	);
 }
