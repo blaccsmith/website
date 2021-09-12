7436dd89527d8b3b735169b95b65f1568052b822
Garrett1Tolbert <garrett1.tolbert@gmail.com>
2021-09-12 01:26:18 +0000

    added gql quey

--- pages/api/resources.ts
+++ pages/api/resources.ts
@@ -1,5 +1,25 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
+const query = gql`
+	query repo($name: String!, $owner: String!) {
+		repository(name: $name, owner: $owner) {
+			url
+			name
+			description
+			stargazerCount
+			repositoryTopics(first: 3) {
+				nodes {
+					topic {
+						name
+					}
+				}
+				totalCount
+			}
+			updatedAt
+		}
+	}
+`;
+
 export default (req: NextApiRequest, res: NextApiResponse) => {
 	switch (req.method) {
 		case 'POST':
@@ -8,6 +28,9 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
 			break;
 		case 'GET':
 			//get all supabase resources
+
+			// pass name and owne as variables to the gql query
+			const [, owner, name] = repo.substring(8).split('/');
 			break;
 		default:
 			res.status(400).json({ message: 'Not found' });
