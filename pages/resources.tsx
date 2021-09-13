c7d654aac9a6913c2240e619d2b3993214b6b080
Garrett1Tolbert <garrett1.tolbert@gmail.com>
2021-09-13 04:25:54 +0000

    refactor: updated resources page

--- pages/resources.tsx
+++ pages/resources.tsx
@@ -1,33 +1,36 @@
-import { GetStaticProps } from 'next';
 import {
 	Box,
 	Button,
 	FormControl,
 	FormHelperText,
 	FormLabel,
+	Grid,
 	Heading,
 	HStack,
 	Input,
 	useToast,
 } from '@chakra-ui/react';
 import axios from 'axios';
-import React, { useState } from 'react';
-import resources from './api/resources';
+import React, { useEffect, useState } from 'react';
+import Repo from '@/components/atoms/Repo';
+import { Repository } from 'types';
 
 interface Props {
 	resources: any;
 }
 
-export default function Resources({ resources }: Props) {
-	const [isError, setIsError] = useState(false);
-	const [repo, setRepo] = useState('');
+export default function Resources() {
 	const toast = useToast();
+	const [repo, setRepo] = useState('');
+	const [repos, setRepos] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [isError, setIsError] = useState(false);
 
 	const handleClick = async () => {
 		if (!repo.startsWith('https://github.com')) setIsError(true);
 		else {
 			setIsError(false);
-			const { data } = await axios.post('/api/resources', {url:repo});
+			const { data } = await axios.post('/api/resources', { url: repo });
 
 			toast({
 				title: data.message,
@@ -38,12 +41,24 @@ export default function Resources({ resources }: Props) {
 		}
 	};
 
+	const fetchData = async () => {
+		setLoading(true);
+		const { data } = await axios.get('/api/resources');
+		console.log(data.resources);
+		setRepos(data.resources);
+		setLoading(false);
+	};
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+
 	return (
-		<Box minHeight="calc(100vh - 218px)" p="16">
+		<Box minHeight="calc(100vh - 218px)" p={{ base: '6', lg: '16' }}>
 			<Heading as="h1" color="brand.white" fontSize="4xl" mb="12">
 				Resources
 			</Heading>
-			<FormControl id="Repo">
+			<FormControl id="Repo" mb="12">
 				<FormLabel color="brand.white">Paste a Repo url</FormLabel>
 				<HStack>
 					<Input
@@ -72,26 +87,11 @@ export default function Resources({ resources }: Props) {
 					Not a valid repo
 				</FormHelperText>
 			</FormControl>
-			{resources.map(({ id, repository }: any) => {
-				return (
-					<Box key={id}>
-						<Box p="3" mb="5" bg="brand.purple.400">
-							{repository.name}
-						</Box>
-					</Box>
-				);
-			})}
+			<Grid rowGap="6" columnGap="3" templateColumns="1fr 1fr">
+				{repos.map(({ repository }, idx) => (
+					<Repo key={idx} data={repository} isLoaded={!loading} />
+				))}
+			</Grid>
 		</Box>
 	);
 }
-
-export const getStaticProps: GetStaticProps = async () => {
-	const { resources }: any = await (
-		await axios.get('http://localhost:3000/api/resources')
-	).data;
-	return {
-		props: {
-			resources,
-		},
-	};
-};
