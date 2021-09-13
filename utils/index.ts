af58ec5cc433ec8fbce1531b7189f3078dedb4e3
Garrett1Tolbert <garrett1.tolbert@gmail.com>
2021-09-13 04:25:23 +0000

    feat: added util to format number to k,m,b

---
+++ utils/index.ts
@@ -0,0 +1,22 @@
+export const convertNum = (num: number) => {
+	if (num < 1000) return num;
+
+	var si = [
+		{ v: 1e3, s: 'K' },
+		{ v: 1e6, s: 'M' },
+		{ v: 1e9, s: 'B' },
+		{ v: 1e12, s: 'T' },
+		{ v: 1e15, s: 'P' },
+		{ v: 1e18, s: 'E' },
+	];
+	var i;
+	for (i = si.length - 1; i > 0; i--) {
+		if (num >= si[i].v) {
+			break;
+		}
+	}
+	return (
+		(num / si[i].v).toFixed(2).replace(/\.0+$|(\.[0-9]*[1-9])0+$/, '$1') +
+		si[i].s
+	);
+};
