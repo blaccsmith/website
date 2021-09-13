2ef45b6f3e06045dd6193e254696f6342d2edbba
Garrett1Tolbert <garrett1.tolbert@gmail.com>
2021-09-13 02:37:44 +0000

    refactor: updated resources endpoint

--- pages/api/resources.ts
+++ pages/api/resources.ts
@@ -1,55 +1,37 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import type { PostgrestResponse } from '@supabase/supabase-js';
-import { supabase, client } from '@/utils/index';
-import { reposQuery } from '../../lib/queries';
+import GraphQL from '@/utils/graphql';
+import { reposQuery } from '@/utils/graphql/queries';
+import Supabase from '@/utils/supabase';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+	const supabase = new Supabase();
+
 	switch (req.method) {
 		case 'POST':
 			const created_at: string = new Date(Date.now()).toISOString();
-			console.log(req.body);
-			const { error: postError }: PostgrestResponse<any> = await supabase
-				.from('resources')
-				.insert([
-					{
-						created_at: created_at,
-						url: req.body.url,
-					},
-				]);
-
-			if (postError !== null) {
-				res.status(400).json({ message: postError });
-			}
-
-			res.status(201).json({
-				message: 'Thanks for the submission',
+			const { error } = await supabase.insert('resources', {
+				created_at: created_at,
+				url: req.body.url,
 			});
+
+			if (error) res.status(500).json({ message: error.message });
+			else res.status(201).json({ message: 'Thanks for the submission' });
 			break;
 		case 'GET':
-			const { data: repos, error: getError }: PostgrestResponse<any> =
-				await supabase.from('resources').select('*');
+			const { data, error: err } = await supabase.getAll('resources');
 
-			if (getError !== null) {
-				res.status(400).json({ message: getError });
-			}
+			if (err) res.status(500).json({ message: err.message });
 
-			const requestHeaders = {
-				authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-			};
-
-			let unresolvedResources: Promise<any>[] | undefined = (
-				repos as any[]
-			).map(async (repo) => {
+			const gql = new GraphQL();
+			const promises = (data as any[]).map(async (repo) => {
 				const [, owner, name] = repo.url.substring(8).split('/');
-				const variables = { owner: owner, name: name };
-
-				return await client.request(
-					reposQuery,
-					variables,
-					requestHeaders
-				);
+				return await gql.runQuery({
+					query: reposQuery,
+					variables: { owner, name },
+				});
 			});
-			const resources = await Promise.all(unresolvedResources);
+
+			const resources = await Promise.all(promises);
 			res.status(200).json({ resources });
 			break;
 		default:
