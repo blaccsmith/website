1eff459d4a08b2100075d160358e26b6965a4b6c
Garrett1Tolbert <garrett1.tolbert@gmail.com>
2021-09-24 02:32:13 +0000

    refactor: updated hook to fetch repos

--- hooks/useResources.ts
+++ hooks/useResources.ts
@@ -2,42 +2,25 @@
 import useSWR from 'swr';
 import { fetcher } from '@/utils/index';
 import { Repository } from 'types';
-import axios from 'axios';
 
 interface ReposResponse {
 	error: any;
 	loading: boolean;
-	resources: {
-		repository: Repository;
-	}[];
+	repos: Repository[];
 }
 
 interface Props {
 	query: string;
-	url: string;
 }
 
-const tempFetcher = async (query: string, url: string) => {
-	try {
-		console.log({ query, url });
-		await axios.get(url);
-		console.log('hi');
-		const res = await fetcher(query, url);
-		console.log({ res });
-		return { data: res };
-	} catch (error) {
-		return { error: 'Repo must be public' };
-	}
-};
+export default function useRepos({ query }: Props): ReposResponse {
+	const { data, error } = useSWR(query, fetcher);
 
-export default function useRepos({ query, url }: Props): ReposResponse {
-	const { data: res } = useSWR(url ? [query, url] : null, tempFetcher);
-
-	console.log({ data: res?.data, error: res?.error });
+	console.log({ data, error });
 
 	return {
-		error: res?.error,
-		loading: !res?.error && !res?.data,
-		resources: res?.data?.resources,
+		error,
+		loading: !error && !data,
+		repos: data?.getRepos,
 	};
 }
