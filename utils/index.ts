8441eadb23b207d8cf858aa227cd8791c9d09fe8
KP <garrett1.tolbert@gmail.com>
2021-09-26 20:12:35 -0500

    refactor: updated fetcher to allow for variables

--- utils/index.ts
+++ utils/index.ts
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { request } from 'graphql-request';
 
 export const api = 'http://localhost:3030/api';
 
-export const fetcher = (query: string) => request(api, query);
+export const fetcher = (query: string, variables?: string) =>
+	request(api, query, variables ? JSON.parse(variables) : undefined);
