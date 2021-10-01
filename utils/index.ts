80cf250744bfcc407c8a8e5f6a65283242552744
Garrett Tolbert <garrett1.tolbert@gmail.com>
2021-09-30 21:03:42 -0500

    refactor: updated api

--- utils/index.ts
+++ utils/index.ts
@@ -1,7 +1,6 @@
 import { request } from 'graphql-request';
 
-// const api = 'https://api-git-staging-blacc.vercel.app/api';
-export const api = 'http://localhost:3030/api';
+const api = 'https://api-git-staging-blacc.vercel.app/api';
 
 export const fetcher = (query: string) => request(api, query);
 
