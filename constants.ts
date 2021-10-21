d5d12cf925ea78c93b10e2667d26f9f88be82864
Garrett1Tolbert <garrett1.tolbert@gmail.com>
2021-10-21 02:37:16 +0000

    chore: added pillars constant

--- constants.ts
+++ constants.ts
@@ -16,3 +16,9 @@ export const homePageImages: string[] = [
 	'https://images.unsplash.com/photo-1573496800440-5c9c48a8d0f0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=776&q=80',
 	'https://images.unsplash.com/photo-1573166953836-06864dc70a21?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=776&q=80',
 ];
+
+export const pillars = [
+	{label:'equity',description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae sollicitudin libero. Sed placerat mollis tortor, sit amet ullamcorper ex fermentum a. Sed mauris lacus, euismod nec tincidunt id, scelerisque eget justo.'},
+	{label:'education',description:'Mauris dapibus tellus faucibus, ultricies tellus et, aliquet libero. Suspendisse a eleifend purus. Maecenas rutrum rhoncus risus, ut tempus odio.'},
+	{label:'collaboration',description:'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer sodales libero ipsum, et pharetra ante tincidunt ut.'}
+]
