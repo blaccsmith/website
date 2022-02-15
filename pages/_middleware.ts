95ff3200f6c2909cc8f40762f1a7c083636d3b06
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-02-15 10:19:29 -0600

    chore: remove response

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -2,8 +2,7 @@ import type { NextFetchEvent, NextRequest } from 'next/server';
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
 	console.log('Edit and run at the edge!');
-
-	return new Response({
+	console.log({
 		ip: req.ip,
 		geo: req.geo, // this will spin the globe!
 		ua: req.ua,
