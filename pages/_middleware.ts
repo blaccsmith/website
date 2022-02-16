cdd19b8d8e8f8f6eb37d71ce617eeec95056c025
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-02-16 01:40:25 -0600

    chore: removed useless code

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -5,10 +5,6 @@ export function middleware(req: NextRequest, ev: NextFetchEvent) {
 	const city = req.geo?.city;
 	const country = req.geo?.country;
 	const res = NextResponse.next();
-	res.cookie('blacc-geo', JSON.stringify({ city, country }));
-
-	// if (city && country) {
 	res.headers.set('X-Geo', `${city}, ${country}`);
-	// }
 	return res;
 }
